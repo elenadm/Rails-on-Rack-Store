4c9ad5daaa1795a3cb3e316558aab0ff901649a8
elenadmitrieva <elena.an.dmitrieva@gmail.com>
2015-02-17 18:31:20 +0200

    Added 404 for not found single product.

--- store.rb
+++ store.rb
@@ -5,7 +5,12 @@ class Store
     q = env['QUERY_STRING']
     name = q.split('=').last
     if name
-      [200, {}, [Product.find(name).to_html]]
+      pr = Product.find(name)
+      if pr
+        [200, {"Content-Type" => "text/html"}, [pr.to_html]]
+      else
+        [404, {"Content-Type" => "text/html"}, ["Product not found"]]
+      end
     else
       [200, {"Content-Type" => "text/html"}, [Product.to_html, 'What name you are looking for?<form><input name="name"><input type="submit"></form>']]
     end
