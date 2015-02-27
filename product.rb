0e04a87acaf509d07e384077d572c979eaf0d920
elenadmitrieva <elena.an.dmitrieva@gmail.com>
2015-02-27 23:25:21 +0200

    Implementation of group work (start working with ERB).

--- product.rb
+++ product.rb
@@ -1,7 +1,9 @@
 require './layout.rb'
+require "erb"
 
 class Product
   include Layout
+  extend Layout
   @@products = []
 
   attr_reader :name
@@ -37,12 +39,14 @@ class Product
 
   def to_html
     layout do
-      "<p><a href='http://localhost:8080/#{self.name}'>#{self.name}:   #{self.price}  <img src='#{self.img}' width='100' height='100' ></p>"
+      "<p><a href='http://localhost:8080/#{self.name}'>#{self.name}:</a>  #{self.price}  <img src='img/#{self.img}' width='100' height='100' ></p>"
     end
   end
 
   def self.to_html
-    @@products.reduce('') { |list, e| list + e.to_html }
+    context = ERBContext.new(:products => Product.all)
+    #p __dir__
+    render("./layout.html.erb", "./products.html.erb", context)
   end
 end
 
@@ -52,3 +56,19 @@ Product.new("hat", 40, "hat.jpg").save
 Product.new("jacket", 55, "jacket.jpg").save
 Product.new("jumper", 25, "jumper.jpg").save
 Product.new("shirt", 20, "shirt.jpg").save
+
+class ERBContext
+  def initialize(hash)
+    hash.each do |k, v|
+      self[k]= v #instance_variable_set("@"+k.to_s,v)
+    end
+  end
+
+  def get_binding
+    binding
+  end
+
+  def []=(k, v)
+    instance_variable_set("@"+k.to_s, v)
+  end
+end
