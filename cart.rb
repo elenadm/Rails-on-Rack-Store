7d04579c032dcfb5b0ebea23f3fb84f172526e7f
elenadmitrieva <elena.an.dmitrieva@gmail.com>
2015-03-09 14:41:44 +0200

    Fixed of carts for many buyers.

--- cart.rb
+++ cart.rb
@@ -1,38 +1,41 @@
 require './erb_context.rb'
 
 class Cart
-  @@cart = {}
+  def initialize
+    @cart = {}
+  end
 
   def save
-    @@cart << self
+    @cart << self
     self
   end
 
   def products
-    @@cart
+    @cart
   end
 
   def add(product)
-    unless @@cart[product]
-      @@cart[product] = 0
+    unless @cart[product]
+      @cart[product] = 0
     end
-    @@cart[product] += 1
+    @cart[product] += 1
   end
 
   def delete(product)
-    @@cart[product] -= 1
-    @@cart.delete_if { |key, value| value == 0 }
+    @cart[product] -= 1
+    @cart.delete_if { |key, value| value == 0 }
   end
 
-  def sumtotal_price
-    @@cart.each do |product, count_product|
-      @price = product.price * count_product
+  def total_price
+    @price = 0
+    @cart.each do |product, count_product|
+      @price += product.price * count_product
     end
     @price
   end
 
   def delete_all
-    @@cart = {}
+    @cart = {}
   end
 
   def to_html
